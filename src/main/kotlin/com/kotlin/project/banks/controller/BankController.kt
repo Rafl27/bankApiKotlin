2a5ac768c4e287846a4013c868980e63c01c64ce
Rafael <rafael.souza@hotmart.com>
2022-12-16 12:21:42 -0300

    controller and test for getAll banks working

---
+++ src/main/kotlin/com/kotlin/project/banks/controller/BankController.kt
@@ -0,0 +1,16 @@
+package com.kotlin.project.banks.controller
+
+import com.kotlin.project.banks.model.Bank
+import com.kotlin.project.banks.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks();
+
+}
