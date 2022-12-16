4ac571a19f46272f8a33835b4ce029fac084ed44
Rafael <rafael.souza@hotmart.com>
2022-12-16 11:14:28 -0300

    changes on Mac

--- src/main/kotlin/com/kotlin/project/banks/service/BankService.kt
+++ src/main/kotlin/com/kotlin/project/banks/service/BankService.kt
@@ -1,8 +1,13 @@
 package com.kotlin.project.banks.service
 
+import com.kotlin.project.banks.datasource.BankDataSource
+import com.kotlin.project.banks.model.Bank
 import org.springframework.stereotype.Service
 
 @Service
-class BankService {
+class BankService (private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> {
+        return dataSource.retrieveBanks()
+    }
 
 }
